2944d5f8c57cf0b9acec042060bbdc953e7f5519
MLKendall <mandy@heysparkbox.com>
2023-11-14 19:53:57 -0500

    feat: draw isochrone contour

--- js/mapScript.js
+++ js/mapScript.js
@@ -21,9 +21,36 @@ async function getIso() {
     { method: 'GET' }
   );
   const data = await query.json();
-  console.log(data);
+    // Set the 'iso' source's data to what's returned by the API query
+    map.getSource('iso').setData(data);
 }
 
-// Call the getIso function
-// You will remove this later - it's just here so you can see the console.log results in this step
-getIso();
+map.on('load', () => {
+    // When the map loads, add the source and layer
+    map.addSource('iso', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: []
+      }
+    });
+  
+    map.addLayer(
+      {
+        id: 'isoLayer',
+        type: 'fill',
+        // Use "iso" as the data source for this layer
+        source: 'iso',
+        layout: {},
+        paint: {
+          // The fill color for the layer is set to a light purple
+          'fill-color': '#5a3fc0',
+          'fill-opacity': 0.3
+        }
+      },
+      'poi-label'
+    );
+  
+    // Make the API call
+    getIso();
+  });
